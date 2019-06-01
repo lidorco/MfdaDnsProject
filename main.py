5eb7b74dc9596d4d4d646350fcb2bbda7cac3e1c
lidorco <lidorco@outlook.com>
2019-06-01 23:36:55 +0300

    Split users queries into fixed-size chunks

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ from data_exploration import explore_data
 
 DATA_PATH = r".\data\raw\*.csv"
 
-NUMBER_OF_QUERIES_EACH_CHUNK = 250
+NUMBER_OF_QUERIES_EACH_CHUNK = 750
 NUMBER_OF_USERS = 15
 LEARNING_USERS = 10
 TEST_UESR = 5
+LEARNING_CHUNKS = 50
 
 
 def collect_data():
@@ -24,25 +25,40 @@ def collect_data():
     return users_data
 
 
-def merge_users_data_to_features_df(users_data):
-    df = pd.concat([ user.loc[user['dns.flags.response'] == 0] for user in users_data]) # get only users queries
-    df = df.reset_index()
-    df = df.set_index(['user', 'frame.number'])
-    return df
+def users_data_to_chunks(users_data):
+    """returns:
+    [
+        [['www.google.com', 'www.facebook.com', ...], [], [], ... []] - user 1
+        [[], [], [], ... []] - user 2
+        [[], [], [], ... []] - user 3
+        ..
+        [[], [], [], ... []] - user 15
+    ]
+    """
+    users_queries = []
+    for user in users_data:
+        users_queries.append(list(user.loc[user['dns.flags.response'] == 0]['dns.qry.name']))
 
+    users_queries_split_by_chunks = []
+    for user_queries in users_queries:
+        number_of_chunks = len(user_queries)
+        user_queries_split_by_chunks = []
+        for i in range(int(number_of_chunks / NUMBER_OF_QUERIES_EACH_CHUNK) + 1):
+            user_queries_split_by_chunks.append(user_queries[i*NUMBER_OF_QUERIES_EACH_CHUNK:(1+i)*NUMBER_OF_QUERIES_EACH_CHUNK])
+        users_queries_split_by_chunks.append(user_queries_split_by_chunks)
 
-
-
+    return users_queries_split_by_chunks
 
 
 def main():
     users_data = collect_data()
 
-    data_df = None # merge_users_data_to_features_df(users_data)
     domains_usage_count_df, valid_domains, suspicious_domains = explore_data(users_data)
+    queries = users_data_to_chunks(users_data)
 
-    df = build_main_df(domains_usage_count_df, valid_domains, suspicious_domains)
+    df = build_main_df(queries, domains_usage_count_df, valid_domains, suspicious_domains)
     pass
+
 
 if __name__ == "__main__":
     main()
