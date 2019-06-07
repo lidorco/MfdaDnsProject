2a20c092c0e0f5312ff2063bddf9a5dffcb5d9d0
lidorco <lidorco@outlook.com>
2019-06-07 16:51:40 +0300

    Check accuracy rate

--- create_classifiers.py
+++ create_classifiers.py
@@ -1,5 +1,6 @@
 from time import time
 
+import numpy as np
 import pandas as pd
 from sklearn.metrics import accuracy_score
 from xgboost import XGBClassifier
@@ -8,13 +9,13 @@ from consts import NUMBER_OF_USERS, LEARNING_CHUNKS, TESTING_CHUNKS, USEABLE_CHU
 
 
 def create_classifiers(features, queries):
-    #Add labels:
+    ### Add labels ###:
     for user_id in range(NUMBER_OF_USERS):
         features['label{}'.format(user_id)] = \
             pd.Series([0] * USEABLE_CHUNKS * user_id + [1] * USEABLE_CHUNKS + [0] * USEABLE_CHUNKS * (NUMBER_OF_USERS - 1 - user_id),
                                                         index=features.index)
 
-    # create train_x train_y, test_x, test_y
+    ### create train_x train_y, test_x, test_y ###
     features = features.reset_index()
 
     train_x = features.loc[features['Chunk'] < LEARNING_CHUNKS].set_index(['User', 'Chunk'])  # features without labels (first 50)
@@ -27,11 +28,16 @@ def create_classifiers(features, queries):
     test_x = features.loc[features['Chunk'] >= LEARNING_CHUNKS].set_index(['User', 'Chunk'])  # features with label (last 40)
     test_y = pd.DataFrame({"label{}".format(i): list(test_x.pop("label{}".format(i))) for i in
                             range(NUMBER_OF_USERS)})  # only labels (last 40)
+    test_y2 = pd.DataFrame(columns=['User', 'Chunk']).set_index(['User', 'Chunk']) # only labels (last 40)
+    for user_id in range(NUMBER_OF_USERS):
+        for chunk_id in range(LEARNING_CHUNKS, TESTING_CHUNKS + LEARNING_CHUNKS):
+            test_y2.loc[(user_id, chunk_id), 'label'] = user_id
     test_x = test_x.reset_index()
     test_x.pop('User')
     test_x.pop('Chunk')
 
-    # train
+
+    ### train ###
     models = []
     t1 = time()
     print("Started XGBClassifier in", t1)
@@ -42,9 +48,33 @@ def create_classifiers(features, queries):
     t2 = time()
     print('Finished XGBClassifier in ', t2, ' Total time ', t2 - t1, ' sec.')
 
-    # test results
+
+    ### test results ###
+
+    # check predictions:
     preds = []
     for i in range(NUMBER_OF_USERS):
         preds.append(models[i].predict_proba(test_x))
 
-    pass # TODO: calculate final prediction results and prediction rate using test_y
+    # check accuracy 1:
+    predictions = pd.DataFrame(columns=["label{}".format(x) for x in range(NUMBER_OF_USERS)])
+    for chunk_number in range(TESTING_CHUNKS * NUMBER_OF_USERS):
+        predicted_user_id = np.argmax([preds[user_id][:,1][chunk_number] for user_id in range(NUMBER_OF_USERS)])
+        new_col = {"label{}".format(x):0 for x in range(NUMBER_OF_USERS)}
+        new_col["label{}".format(predicted_user_id)] = 1
+        predictions = predictions.append(new_col, ignore_index=True)
+
+    predictions.eq(test_y.values).mean() # calculate accuracy for each user separately
+
+    # check accuracy 2:
+    predictions2 = pd.DataFrame(columns=['User', 'Chunk']).set_index(['User', 'Chunk'])
+    for user_id in range(NUMBER_OF_USERS):
+        for chunk_id in range(LEARNING_CHUNKS, TESTING_CHUNKS + LEARNING_CHUNKS):
+            real_chunk_id = chunk_id-LEARNING_CHUNKS + user_id * TESTING_CHUNKS
+            predicted_user_id = \
+                np.argmax([preds[i][:,1][real_chunk_id] for i in range(NUMBER_OF_USERS)])
+            predictions2.loc[(user_id, chunk_id), 'label'] = predicted_user_id
+
+    accuracy = accuracy_score(predictions2, test_y2)
+    print("Accuracy: %.2f%%" % (accuracy * 100.0))
+    pass
